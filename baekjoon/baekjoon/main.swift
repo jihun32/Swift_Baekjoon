583f0f77cc9a9eca539f656cc0fff07f29aea104
jihun32 <jihun32@naver.com>
2022-08-25 17:15:07 +0900

    Queue

--- baekjoon/baekjoon/main.swift
+++ baekjoon/baekjoon/main.swift
@@ -1,11 +1,65 @@
-//
-//  main.swift
-//  baekjoon
-//
-//  Created by 정지훈 on 2022/08/18.
-//
 
 import Foundation
 
-print("Hello, World!")
+struct Queue<T> {
+    var queue: [T] = []
+    
+    var size: Int {
+        return queue.count
+    }
+    var empty: Bool {
+        return queue.isEmpty
+    }
+    var front: T {
+        return queue.first!
+    }
+    var back: T {
+        return queue.last!
+    }
+    mutating func push(_ element: T) {
+        queue.append(element)
+    }
+    mutating func pop() -> Int {
+        return queue.removeFirst() as! Int
+    }
+}
 
+let n = Int(readLine()!)!
+var queue = Queue<Int>()
+
+for _ in 0..<n {
+    let command = readLine()!.split(separator: " ")
+    
+    switch command[0] {
+    case "size" :
+        print(queue.size)
+    case "empty" :
+        if queue.empty {
+            print("1")
+        } else {
+            print("0")
+        }
+    case "front" :
+        if queue.empty {
+            print("-1")
+        } else {
+            print(queue.front)
+        }
+    case "back" :
+        if queue.empty {
+            print("-1")
+        } else {
+            print(queue.back)
+        }
+    case "pop" :
+        if queue.empty {
+            print("-1")
+        } else {
+            print(queue.pop())
+        }
+    case "push" :
+        queue.push(Int(command[1])!)
+    default :
+        break
+    }
+}
